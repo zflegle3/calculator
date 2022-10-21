95c4bc1b067df0422bf6864da86f3440f4f5e96e
Zachary Flegle <zflegle3@gmail.com>
2022-10-21 01:05:59 -0400

    Completed challenge including, functionality and styling

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function operate(num1,num2,opp) {
         case '-':
             calcData.result = subtract(num1,num2);
             break;
-        case '*':
+        case 'x':
             calcData.result = multiply(num1,num2);
             break;
         case '/':
@@ -69,10 +69,18 @@ function updateDisplay() {
     //updates display to reflect equationDisplay variable from calcData object
     //either result if operate has populated the result or unsolved equation
     if (calcData.result !== "") {
-        displayElement.textContent = calcData.result;
+        displayElement.textContent = parseFloat(calcData.result).toLocaleString('en');
         //should call update display and create var 1 as the result 
     } else {
-        displayElement.textContent = calcData.equationDisplay;
+        if (calcData.equationDisplay !== "") {
+            if (calcData.var2 !== "") {
+                displayElement.textContent = `${parseFloat(calcData.var1).toLocaleString('en')} ${calcData.opp1} ${parseFloat(calcData.var2).toLocaleString('en')}` ;
+            } else {
+                displayElement.textContent = `${parseFloat(calcData.var1).toLocaleString('en')} ${calcData.opp1}`;
+            }
+        } else {
+            displayElement.textContent = "0";
+        }
     }
 }
 
@@ -87,7 +95,6 @@ function updateEq(tempCharacter,varSelected) {
 function removeLast() {
     let lastClickType = calcData.allUserInType[calcData.allUserIn.length-1] 
     if (calcData.allUserIn.length !== 0) {
-        console.log("Back Works")
         calcData[lastClickType] = calcData[lastClickType].slice(0,-1); //remove from last stored variable
         calcData.equationDisplay = calcData.equationDisplay.slice(0,-1); //remove from equation display variable
         calcData.allUserIn.pop(); //remove character from user inputs
@@ -96,85 +103,111 @@ function removeLast() {
     updateDisplay();
 }
 
-function sort() {
-    let tempUserInEle = calcData.allUserIn[calcData.allUserIn.length-1]; //selects temp element from latest click event selected by user
-    //checks 1st class of selected element to differentiates type of button selected and determines action
-    switch (tempUserInEle.className.split(" ")[0]) { 
-        case "num":
-            if (calcData.opp1 !== "") {
-                //expects numbers to be entered before opperators, if opp DNE var1 if opp exists var 2
-                updateEq(tempUserInEle.textContent,"var2");
-            } else {
-                updateEq(tempUserInEle.textContent,"var1");
-            }
-            break;
-        case "opp":
-            if (calcData.opp1 === "" && calcData.var1 !== "") {
-                //if opp DNE but var1 exists populates opp
-                updateEq(tempUserInEle.textContent,"opp1");
-            } else if (calcData.opp1 !== "" && calcData.var2 !== "") {
-                //else executes eq with current variables and sets up new equation with result as var 1 and new opp
-                updateEq(tempUserInEle.textContent,"opp2"); //adds new click as opp2 variable 
-                operate(Number(calcData.var1), Number(calcData.var2), calcData.opp1); //calls operate with opp1 and variables 
-            }
-            break;
-        case "eq":
-            if (calcData.opp1 !== "" && calcData.var2 !== "") {
-                //if all variables needed have been populated calls operate w/ variables,
-                //updates display, and resets the eq except result variable which is displayed
-                operate(Number(calcData.var1), Number(calcData.var2), calcData.opp1);
-            }
-            break;
-        case "back":
-            //calls remove last when pressed
-            calcData.allUserIn.pop(); //removing backspace entry
-            removeLast();
-            break;
-        case "clear":
-            //clears all variables and updates display
-            resetEq();
-            calcData.result = "";
-            updateDisplay();
-            break;
-        case "dec":
-            if (calcData.opp1) {
-                //decimal to var2
-                if (!calcData.var2.includes(".") ) {
-                    console.log("Hello2")
-                    updateEq(tempUserInEle.textContent,"var2")
-                }
-            } else {
-                //decimal to var 1
-                if (!calcData.var1.includes(".")) {
-                    console.log("Hello1")
-                    updateEq(tempUserInEle.textContent,"var1")
-                }
-            }
-            break;
-    }
-}
-
-function userIn(e) {
-    e.stopPropagation();
-    calcData.allUserIn.push(e.srcElement);//adds each srcElement clicked to an array in CalcData
-    sort() //calls sort to sort through the click type 
-    console.log(calcData);
-}
-
 function styleIn(e) {
     e.stopPropagation();
-    console.log(e.target.id);
     document.getElementById("select-ball").classList = e.target.id;
     document.getElementById("body").classList = e.target.id;
 }
 
+function numIn(e) {
+    e.stopPropagation();
+    //add each srcElement clicked to an array in CalcData
+    calcData.allUserIn.push(e.srcElement);
+    let numIn = e.target.textContent;
+    if (calcData.opp1 !== "") {
+        //expects numbers to be entered before opperators, if opp DNE var1 if opp exists var 2
+        updateEq(numIn,"var2");
+    } else {
+        updateEq(numIn,"var1");
+    }
+}
+
+function oppIn(e) {
+    e.stopPropagation();
+    //add each srcElement clicked to an array in CalcData
+    calcData.allUserIn.push(e.srcElement);
+    let oppIn = e.target.textContent;
+
+
+    if (calcData.opp1 === "" && calcData.var1 !== "") {
+        //if opp DNE but var1 exists populates opp
+        updateEq(oppIn,"opp1");
+    } else if (calcData.opp1 !== "" && calcData.var2 !== "") {
+        //else executes eq with current variables and sets up new equation with result as var 1 and new opp
+        updateEq(oppIn,"opp2"); //adds new click as opp2 variable 
+        operate(Number(calcData.var1), Number(calcData.var2), calcData.opp1); //calls operate with opp1 and variables 
+    }
+}
+
+function equalsIn(e) {
+    e.stopPropagation();
+    //add each srcElement clicked to an array in CalcData
+    calcData.allUserIn.push(e.srcElement);
+    let eqIn = e.target.textContent;
+    if (calcData.opp1 !== "" && calcData.var2 !== "") {
+        //if all variables needed have been populated calls operate w/ variables,
+        //updates display, and resets the eq except result variable which is displayed
+        operate(Number(calcData.var1), Number(calcData.var2), calcData.opp1);
+    }
+}
+
+function delIn(e) {
+    e.stopPropagation();
+    // calcData.allUserIn.pop(); //removing backspace entry
+    removeLast();
+}
+
+function clearIn(e) {
+    e.stopPropagation();
+    resetEq();
+    calcData.result = "";
+    updateDisplay();
+}
+
+function decIn(e) {
+    e.stopPropagation();
+    let decIn = e.target.textContent;
+    if (calcData.opp1) {
+        //decimal to var2
+        if (!calcData.var2.includes(".") ) {
+            updateEq(decIn,"var2")
+        }
+    } else {
+        //decimal to var 1
+        if (!calcData.var1.includes(".")) {
+            updateEq(decIn,"var1")
+        }
+    }
+}
+
+const getDisplayNumber = (number) => {
+    let floatNumber = parseFloat(number);
+    if (isNaN(floatNumber)) return "0"
+    return number.toLocaleString("en");
+}
+
 //DISPLAY WINDOW 
 let displayOutput = "";
-const displayElement = document.querySelector(".window");
+const displayElement = document.querySelector(".calc-window");
 
 //BUTTONS
-const btnsAll = document.querySelectorAll(".btn");
-btnsAll.forEach(individualBtn => individualBtn.addEventListener("click",userIn));
+const numsAll = document.querySelectorAll(".btn-num");
+numsAll.forEach(individualBtn => individualBtn.addEventListener("click",numIn));
+
+const oppsAll = document.querySelectorAll(".btn-opp");
+oppsAll.forEach(individualBtn => individualBtn.addEventListener("click",oppIn));
+
+const equalsBtn = document.querySelector(".btn-equals");
+equalsBtn.addEventListener("click",equalsIn);
+
+const delBtn = document.querySelector(".btn-del");
+delBtn.addEventListener("click",delIn);
+
+const clearBtn = document.querySelector(".btn-clear");
+clearBtn.addEventListener("click",clearIn);
+
+const decimalBtn = document.querySelector(".btn-dec");
+decimalBtn.addEventListener("click",decIn);
  
 const styleBtns = document.querySelectorAll(".style-select");
 styleBtns.forEach(individualBtn => individualBtn.addEventListener("click",styleIn));
